5ceb9c52b05c6a8c671024dff5084d22be827052
StoneyShou <stoneyshow@gmail.com>
2016-08-18 11:38:03 +1000

    remove invisible question

--- js/dev-script.js
+++ js/dev-script.js
@@ -258,7 +258,7 @@ function LEI() {
                                                     <td></td>\
                                                 </tr>\
                                                 <tr class='LEIUtilButtonsRow'>\
-                                                    <td><input class='LEILable' type='checkbox' id='incInv'>Include invisible questions</td>\
+                                                    <td><input style='color:#fff' type='checkbox' id='incInv'>Include invisible questions</td>\
                                                     <td><input type='button' value='Scrape Survey' id='surveyScraper' onclick='lpMTagDebug.scrapeSurvey()'></td>\
                                                     <td><input type='button' value='T.B.D' id='xx'></td>\
                                                 </tr>\
